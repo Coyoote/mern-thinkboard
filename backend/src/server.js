ee12cc44ad58e1676c7ff22a7b79d08941ebec5d
Yurii Demkiv <yurii.demkiv.dev@gmail.com>
2025-12-26 15:57:46 +0200

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+import noteRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json()); // parses JSON bodies
+app.use(rateLimiter); // rate limiter
+
+// routes
+app.use("/api/notes", noteRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+  });
+});
