8f6773c2723728809eb00d63d86b5a3994be0cf8
Yurii Demkiv <yurii.demkiv.dev@gmail.com>
2025-12-26 16:19:18 +0200

    prep for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,18 +11,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+}
 app.use(express.json()); // parses JSON bodies
 app.use(rateLimiter); // rate limiter
 
 // routes
 app.use("/api/notes", noteRoutes);
+
+// deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
